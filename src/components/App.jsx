b210a5d2caa0e8ba807c8d1193bd4b021df6eba8
Tom Spitz <tjspitz1986@gmail.com>
2023-01-03 19:34:55 -0600

    Minor edits to get VideoPlayer and VideoList rendering correctly.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,26 @@
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        < Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes asdf</h5></div>
+        < VideoPlayer video={exampleVideoData[0]} />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        < VideoList videos={exampleVideoData} />
       </div>
     </div>
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
 export default App;
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
